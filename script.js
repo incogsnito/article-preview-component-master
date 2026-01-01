22f758224b70707afb9b340b3e10e5b9a05d00b3
Samad <Samad.o@outlook.com>
2026-01-01 01:33:24 -0500

    Mobile layout complete!!!

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const share = document.querySelector('.btn');
 const bottom = document.querySelector('.bottom');
-
+const article = document.querySelector('.article')
 share.addEventListener('click', () =>{
   bottom.classList.toggle("active");
+  article.classList.toggle("active");
 })
